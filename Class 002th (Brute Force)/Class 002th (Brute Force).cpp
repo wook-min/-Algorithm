4bdb581f3a88ef01ae7ba32e864db2971b175f51
wook-min <gaemicgr@gmail.com>
2025-08-12 21:42:16 +0900

    (2025.8.12.) 시간 복잡도 & 공간 복잡도
    강의 1
    시간 복잡도와 공간 복잡도, 점근적 표기법
    brute Force (가장 기초적인 알고리즘 순차 대입법으로 문제를 해결)

---
+++ Class 002th (Brute Force)/Class 002th (Brute Force).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 완전 탐색
+	// 가능한 모든 경우의 수를 탐색하면서 결과를 찾아내는 알고리즘입니다.
+
+	int password[] = { 9, 1, 9 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				if (password[0] == i && password[1] == j && password[2] == k)
+				{
+					cout << "Release : " << i << " " << j << " " << k << endl;
+					return 0;
+				}
+			}
+		}
+	}
+#pragma endregion
+
+	return 0;
+}
