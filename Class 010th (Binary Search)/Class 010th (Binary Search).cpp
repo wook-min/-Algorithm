611e6a5babb7a515baaef4fd9d7c50799610b0d6
wook-min <gaemicgr@gmail.com>
2025-08-20 21:30:13 +0900

    (2025.08.20.) 분할 정복
    강의 6
    이분 탐색 구현

---
+++ Class 010th (Binary Search)/Class 010th (Binary Search).cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void search(int list[], int key, int size)
+{
+	int left = 0;
+	int right = size - 1;
+	bool check = false;
+
+	while (left <= right)
+	{
+		int pivot = (left + right) / 2;
+
+		if (list[pivot] == key)
+		{
+			cout << "The Key index is " << pivot;
+			check = true;
+			break;
+		}
+		else
+		{
+			if (list[pivot] > key)
+			{
+				right = pivot - 1;
+			}
+			else
+			{
+				left = pivot + 1;
+			}
+		}
+	}
+
+	if (!check)
+	{
+		cout << "Key not found in List" << endl;
+	}
+
+}
+
+int main()
+{
+#pragma region 이분 탐색(Two Pointer Algorithm)
+	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
+	// 좁혀나가는 방식으로 동작하는 알고리즘입니다.
+
+	// 여기서 two pointer란 해당하는 변수의 index를 가리키는 것.
+	// left, right로 배열의 각 index를 가리키며, left는 오른쪽으로 right는 왼쪽으로 이동
+	// 이때 right가 left보다 작아질 때 이를 '엇갈림'이라 표현하며, 이때 탐색을 종료한다.
+
+	// 탐색 시간복잡도 O(log n)
+
+	// 1 2 15 19 33 45 60
+
+	int list[] = { 5, 6, 11, 13, 27, 55, 66, 92 };
+
+	int size = sizeof(list) / sizeof(list[0]);
+
+	search(list, 66, size);
+
+#pragma endregion
+
+	return 0;
+}
--- Prograam/Prograam.cpp
+++ Prograam/Prograam.cpp
@@ -2,44 +2,11 @@
 
 using namespace std;
 
-int Find(int list[], int left, int right)
-{
-	if (left == right)
-	{
-		return list[left];
-	}
-
-	int count = (right + left) / 2;
-
-	int leftMax = Find(list, left, count );
-	int rightMax = Find(list, count + 1, right);
-
-	return (leftMax > rightMax) ? leftMax : rightMax;
-}
 
 int main()
 {
-#pragma region 분할 정복(Divide & Conquer)
-	// 주어진 2개 이상의 부분으로 문제를 나눈 뒤 각 부분 문제에 대한 답을
-	// 재귀로 호출하여 계산한 다음, 그 답으로부터 전체 문제의 답을
-	// 계산해내는 알고리즘입니다.
+	int x = 30;
 
-	// 분할 (Divide) : 주어진 문제를 두 개 혹은 그 이상의 형식으로 나눈다.
-	
-	// 정복 (Conquer) : 나누어진 문제를 재귀적으로 해결해서 나누어진 문제를
-	// 더 이상 나누어서 문제가 필요없을때까지 계속 분할합니다.
-
-	// 통합 (Combine) : 나누어서 해결한 문제들을 통합해서 원래 문제의 해답을 생성합니다.
-
-	// 문제를 푸는 알고리즘이기 때문에, 문제의 종류에 따라 시간복잡도가 다르게 잡힘.
-	// 궁금하면 마스터 정렬 찾아보기.
-
-	int list[] = { 20, 15, 99, 1, 7, 100, 55, 109 };
-
-	cout << "Max Value of List : " << Find(list, 0, 7) << endl;
-
-#pragma endregion
-	
 
 
 	return 0;
